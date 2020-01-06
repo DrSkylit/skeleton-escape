f5a69aa4d452dda0b7d9f10916adfea4d3fa7234
DrSkylit <chris.dolce@aggiemail.usu.edu>
2020-01-05 21:59:38 -0700

    added player movent with the arrow keys, changed some animation keys and names

--- Player/PlayerParent.js
+++ Player/PlayerParent.js
@@ -3,11 +3,13 @@ class PlayerParent extends Phaser.Physics.Arcade.Sprite {
 	    super(scene,x,y,textureKey,image);
 	    this.scene = scene;
 	    this.life = 3;
-
+        this.vX = 100;
+        this.vY = 100;
 	    this.scene.add.existing(this);
 	    this.scene.physics.add.existing(this);
 	    this.body.immovable=true;
 	    this.body.setCollideWorldBounds(true);
+        this.cursors = this.scene.input.keyboard.createCursorKeys();
 	    this.createAnimations();
 	    this.playIdleAnimation();
   	}
@@ -23,9 +25,51 @@ class PlayerParent extends Phaser.Physics.Arcade.Sprite {
   		console.log("function playIdleAnimation needs to be called");
   	}
 
+    playWalkingAnimation(){
+        console.log("function playWalkingAnimation needs to be called");
+    }
+
   	setStartingPosition(map){
       const spawnPoint = map.findObject("spawner", obj => obj.name === "PlayerSpawn");
       this.x = spawnPoint.x
       this.y = spawnPoint.y
+    }
+
+    update(time, delta){
+
+        this.body.setVelocityY(0);
+        this.body.setVelocityX(0);
+
+        if (this.cursors.left.isDown) {
+            this.setFlipX(true);
+            this.body.setVelocityX(-this.vX);
+            this.playWalkingAnimation();
+        } else if (this.cursors.right.isDown) {
+            this.setFlipX(false);
+            this.body.setVelocityX(this.vX);
+            this.playWalkingAnimation();
+        }
+        if (this.cursors.up.isDown) {
+            this.body.setVelocityY(-this.vY);
+              this.playWalkingAnimation();
+        } else if (this.cursors.down.isDown) {
+            this.body.setVelocityY(this.vY);
+            this.playWalkingAnimation();
+        }
+
+        if(this.cursors.left.isUp && this.cursors.right.isUp && this.cursors.up.isUp && this.cursors.down.isUp){
+            // Stop any previous movement from the last frame
+            this.body.setVelocity(0);
+            this.playIdleAnimation();
+        }
+
+        // Normalize and scale the velocity so that player can't move faster along a diagonal
+        this.body.velocity.normalize().scale(this.vX);
+
+    }
+
+    preUpdate(time, delta){
+      super.preUpdate(time, delta);
+      this.update(time, delta);
     }
 }
--- Player/Skeleton.js
+++ Player/Skeleton.js
@@ -3,14 +3,26 @@ class Skeleton extends PlayerParent{
 	    super(0,0,scene,"playerAtlas","skelet_idle_anim_f0.png");
 	}
     createAnimations(){
-		var skelIdle = this.scene.anims.generateFrameNames('playerAtlas', {
+		var idle = this.scene.anims.generateFrameNames('playerAtlas', {
 	        start: 0, end: 3,
 	        prefix: 'skelet_idle_anim_f', suffix: '.png'
 	    });
 
+      	var walk = this.scene.anims.generateFrameNames('playerAtlas', {
+           	start: 0, end: 3,
+           	prefix: 'skelet_run_anim_f', suffix: '.png'
+       	});
+
 		this.scene.anims.create({
 	        key: 'skeletonIdle',
-	        frames: skelIdle,
+	        frames: idle,
+	        frameRate: 8,
+	        repeat: -1
+	    });
+
+	    this.scene.anims.create({
+	        key: 'skeletonWalk',
+	        frames: walk,
 	        frameRate: 8,
 	        repeat: -1
 	    });
@@ -18,4 +30,8 @@ class Skeleton extends PlayerParent{
   	playIdleAnimation(){
   		this.play('skeletonIdle',true);
   	}
+
+  	playWalkingAnimation(){
+        this.play('skeletonWalk',true);
+    }
 }
