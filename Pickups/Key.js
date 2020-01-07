1efb9ca590beb56fc6fa64b288ac6c99bebd32a1
DrSkylit <chris.dolce@aggiemail.usu.edu>
2020-01-06 22:05:14 -0700

    Added collision detection for the key and door. Door now open when the key is close

--- Pickups/Key.js
+++ Pickups/Key.js
@@ -1,7 +1,6 @@
 class Key extends PickupParent{
 	constructor (scene) {
 	    super(0,0,scene,"pickupAtlas","key.png");
-	    this.scene = scene;
 	    this.picked = false;
 	    this.player = null;
 	}
@@ -12,11 +11,23 @@ class Key extends PickupParent{
 		})
 		key.picked = true;
 		key.player = player;
+		key.x = player.x;
+		key.y = player.y;
 		key.scene.physics.world.removeCollider(pickupCollision);
+	}
+
+	openDoor(key,door){
+		door.layer.tilemapLayer.setAlpha(0);
+        var closedDoorCollision = key.scene.physics.world.colliders.getActive().find(function(i){
+    		return i.name == 'closedDoor'
+		})
+		key.scene.physics.world.removeCollider(closedDoorCollision);
+        key.destroy();
 	}
 
 	update(time, delta){
 		if(this.picked == true){
+			this.body.velocity = this.player.body.velocity;
 			this.x = this.player.x;
 			this.y = this.player.y;
 			this.angle = 0;
--- scenes/MainGameScene.js
+++ scenes/MainGameScene.js
@@ -19,8 +19,8 @@ class MainGameScene extends Phaser.Scene{
 	    const lowerWallsLayer = map.createStaticLayer("wall_layer0", tileset, 0, 0);
 	    const upperWallsLayer = map.createStaticLayer("wall_layer1", tileset, 0, 0);
 	    upperWallsLayer.setDepth(99);
-	    const doorClosedLayer = map.createStaticLayer("door_open", tileset, 0, 0);
-	    const doorOpenLayer = map.createStaticLayer("door_closed", tileset, 0, 0);
+	    const doorClosedLayer = map.createStaticLayer("door_closed", tileset, 0, 0);
+	    const doorOpenLayer = map.createStaticLayer("door_open", tileset, 0, 0);
 
 	    upperWallsLayer.setCollisionByProperty({ collides: true });
       	lowerWallsLayer.setCollisionByProperty({ collides: true });
@@ -34,8 +34,12 @@ class MainGameScene extends Phaser.Scene{
 
 		this.physics.add.collider(this.player, upperWallsLayer);
       	this.physics.add.collider(this.player, lowerWallsLayer);
+      	var closedDoor = this.physics.add.collider(this.player, doorClosedLayer);
+      	closedDoor.setName("closedDoor");
       	var pickup = this.physics.add.collider(this.player, key, key.pickup);
       	pickup.setName("keyPickup");
+
+      	this.physics.add.collider(key,doorClosedLayer,key.openDoor);
 
 	}
 	//update assets
